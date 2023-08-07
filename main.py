2b7cb00725653db133dd06b2ce8e982d4418624c
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2023-08-07 10:40:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-tab = ""
+tabs = 5
+
+
+def folder_is_zero(prefix):
+    try:
+        if int(prefix[1:-1])==0:
+            return True
+    except:
+        return False
+    
+def manual_tab_count(tabs_original):
+    cancel = "q"
+    print("Overwriting tabs, enter {} to cancel.".format(cancel))
+    while(True):
+        tabs = input("Enter number of tabs: ")
+        if tabs==cancel:
+            return tabs_original
+        
+        if not tabs.isdigit():
+            print("invalid format")
+            continue
+        tabs = int(tabs)
+        if tabs<0:
+            print("invalid format")
+            continue
+        return tabs
+
+
+
 
 while(True):
     prefix = input("Enter prefix (for example F1S, F etc.): ").upper()
@@ -8,7 +36,10 @@ while(True):
         continue
 
     if "S" in prefix:
-        tab="{Tab}"
+        if folder_is_zero(prefix):
+            print("invalid format")
+            continue
+        tabs=6
     break
 
 sub = ""
@@ -35,17 +66,24 @@ while(True):
         print("invalid format")
         continue
     fst_f = int(fst_f)
-    if fst_f<1:
+    if fst_f<0:
         print("invalid format")
         continue
+    if fst_f==0:
+        if "S" in prefix:
+            input("\nError: first folder is 0 and there are subfolders.\nPress enter to exit.")
+            raise Exception()
+        tabs=4
     f_offset = fst_f-1
     break
 
 
-
-
-
 while(True):
+    if fst_f==0:
+        print("Tagging F0 only.")
+        f_n=1
+        break
+
     f_n = input("How many {}folders to tag? ".format(sub))
     if f_n.isdigit():
         f_n = int(f_n)
@@ -56,6 +94,9 @@ f_qs = []
 for f_i in range(1, f_n+1):
     while(True):
         f_q = input("How many questions in {}{} to tag? ".format(prefix, f_i+f_offset))
+        if f_q=="tabstabs":
+            tabs = manual_tab_count(tabs_original=tabs)
+            continue 
         if f_q.isdigit():
             f_q = int(f_q)
             break
@@ -69,7 +110,7 @@ f.close()
 
 main = ""
 for i, q_count in enumerate(f_qs):
-    main += loop.replace("*q count*", str(q_count)).replace("*(sub)folder*", str(i+1+f_offset)).replace("*extra tab*", tab).replace("*prefix*", prefix).replace("*q offset*", q_offset) + "\n"
+    main += loop.replace("*q count*", str(q_count)).replace("*(sub)folder*", str(i+1+f_offset)).replace("*tabs*", "{Tab}"*tabs).replace("*prefix*", prefix).replace("*q offset*", q_offset) + "\n"
     if i==0:
         q_offset="0"
 
--- tagger.ahk
+++ tagger.ahk
@@ -3,37 +3,12 @@
 
 ; to use, run the script then (within 3 seconds) click on the first question in the folder.
 Sleep, 3000
-Loop, 5
+Loop, 4
 {
-	Prefix = F1
+	Prefix = F1S11
 	Qindex := A_index+0
 	Suffix = Q%Qindex%
-	Send, {Tab}{Tab}{Tab}{Tab}{Tab}
-	Send, {Enter}
-	Sleep, 500
-	Send, {Home}
-    Send, %Prefix%%Suffix%
-	SendEvent, {Space}
-	Sleep, 1000
-	Send, {Shift Down}{Tab}{Tab}{Shift Up}
-	Send, {Enter}		
-	Sleep, 6000
-	Send, {Tab}{Enter}
-	Sleep, 200
-	Send, {Tab}{Tab}{Tab}{Tab}
-	Send, {Down}{Enter}
-	Sleep, 1000
-}
-Send, {Down}
-Sleep, 200
-Send, {Enter}
-Sleep, 1000
-Loop, 10
-{
-	Prefix = F2
-	Qindex := A_index+0
-	Suffix = Q%Qindex%
-	Send, {Tab}{Tab}{Tab}{Tab}{Tab}
+	Send, {Tab}{Tab}{Tab}{Tab}{Tab}{Tab}*
 	Send, {Enter}
 	Sleep, 500
 	Send, {Home}
